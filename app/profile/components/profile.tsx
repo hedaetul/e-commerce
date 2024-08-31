22d205161b9452de0f0ab5654f7a02699afe7a19
hedaetul <hedaetulislam143@gmail.com>
2024-08-31 15:29:33 +0600

    testing

---
+++ app/profile/components/profile.tsx
@@ -0,0 +1,158 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useAuth } from "@/context/AuthContext";
+import { countOrders, getPersonalDetails } from "@/lib/firebaseUtils";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { FaUser } from "react-icons/fa";
+
+const Profile = () => {
+  const { user, logout } = useAuth();
+  const [personalDetails, setPersonalDetails] = useState<any>(null);
+  const [orderCount, setOrderCount] = useState(0);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const router = useRouter()
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+      router.push("/");
+    } catch (error) {
+      console.log("Logout failed", error);
+    }
+  };
+
+  useEffect(() => {
+    const fetchPersonalDetails = async () => {
+      try {
+        const details = await getPersonalDetails();
+        setPersonalDetails(details);
+        setError(null);
+      } catch (error) {
+        setError("Failed to fetch personal details");
+        console.log(error);
+      }
+    };
+
+    const fetchOrderCount = async () => {
+      try {
+        const count = await countOrders();
+        setOrderCount(count);
+        setError(null);
+      } catch (error) {
+        setError("Failed to count orders");
+        console.log(error);
+      }
+    };
+
+    const loadData = async () => {
+      setLoading(true);
+      await Promise.all([fetchPersonalDetails(), fetchOrderCount()]);
+      setLoading(false);
+    };
+
+    if (user) {
+      loadData();
+    } else {
+      setLoading(false);
+    }
+  }, [user]);
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center">
+        <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-rose-500 border-t-transparent"></div>
+      </div>
+    );
+  }
+
+  if (error) {
+    return <div className="text-rose-600">Error: {error}</div>;
+  }
+
+  return (
+    <div className="min-h-screen">
+      <div className="mx-auto max-w-4xl rounded-lg p-6">
+        {/* Header */}
+        <div className="mb-6 flex items-center justify-between">
+          <div className="flex items-center">
+            <FaUser className="mr-2 text-2xl text-red-500" />
+            <h1 className="text-2xl font-bold">My Profile</h1>
+          </div>
+          <button className="rounded-lg bg-rose-200 px-4 py-2 text-rose-600 hover:bg-rose-500 hover:text-white">
+            Edit profile
+          </button>
+        </div>
+
+        <div className="mb-6 flex items-center rounded-lg bg-gray-50 p-4">
+          <Image
+            width="100"
+            height="100"
+            src={personalDetails?.photoUrl || <FaUser />}
+            alt="User"
+            className="mr-4 h-20 w-20 rounded-full"
+          />
+          <div>
+            <h2 className="text-xl font-semibold">
+              {personalDetails?.name || "Name not found"}
+            </h2>
+            <p className="text-gray-500">SILVER USER</p>
+            <p className="text-red-500">Balance: $500</p>
+          </div>
+        </div>
+
+        <div className="mb-6 grid grid-cols-4 gap-4">
+          <div className="rounded-lg bg-white p-4 text-center shadow">
+            <h3 className="text-2xl font-bold text-red-500">{orderCount}</h3>
+            <p className="text-gray-500">All Orders</p>
+          </div>
+          <div className="rounded-lg bg-white p-4 text-center shadow">
+            <h3 className="text-2xl font-bold text-red-500">02</h3>
+            <p className="text-gray-500">Awaiting Payments</p>
+          </div>
+          <div className="rounded-lg bg-white p-4 text-center shadow">
+            <h3 className="text-2xl font-bold text-red-500">00</h3>
+            <p className="text-gray-500">Awaiting Shipment</p>
+          </div>
+          <div className="rounded-lg bg-white p-4 text-center shadow">
+            <h3 className="text-2xl font-bold text-red-500">01</h3>
+            <p className="text-gray-500">Awaiting Delivery</p>
+          </div>
+        </div>
+
+        <div className="rounded-lg bg-gray-50 p-4">
+          <div className="grid grid-cols-2 gap-4">
+            <div>
+              <p className="text-gray-500">Name</p>
+              <p className="text-lg">
+                {personalDetails?.name || "Not Available"}
+              </p>
+            </div>
+            <div>
+              <p className="text-gray-500">Email</p>
+              <p className="text-lg">
+                {personalDetails?.email || "Not Available"}
+              </p>
+            </div>
+            <div>
+              <p className="text-gray-500">Login Method</p>
+              <p className="text-lg">
+                {personalDetails?.loginMethod || "Not Available"}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {user && (
+        <div className="flex w-full justify-end pr-12">
+          <Button onClick={handleLogout}>Logout</Button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Profile;
