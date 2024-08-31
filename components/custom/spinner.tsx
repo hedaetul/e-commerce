27df5b344abdecd8ec2d4fea101c36670d7848e1
hedaetul <hedaetulislam143@gmail.com>
2024-08-31 19:48:03 +0600

    add spinner in login state

---
+++ components/custom/spinner.tsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-rose-500 border-t-transparent"></div>
+    </div>
+  );
+};
+
+export default Spinner;
