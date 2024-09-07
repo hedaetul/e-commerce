8176b6e04f7d00fccaca677bb06f6c0bc17c6136
hedaetul <hedaetulislam143@gmail.com>
2024-09-07 12:43:20 +0600

    refactor && modified vendor acco8unt

--- app/vendor-account/components/sidebarMenu.tsx
+++ app/vendor-account/components/sidebarMenu.tsx
@@ -1,6 +1,13 @@
+"use client";
+
+import { useParams, usePathname, useRouter } from "next/navigation";
 import { FaBox, FaCog, FaShoppingCart, FaTachometerAlt } from "react-icons/fa";
+import { MdOutlineFileUpload } from "react-icons/md";
 
 const SidebarMenu = () => {
+  const router = useRouter();
+  const pathName = usePathname()
+
   return (
     <div className="rounded-md bg-white p-4 shadow-lg">
       <div className="mb-8">
@@ -14,6 +21,10 @@ const SidebarMenu = () => {
           <FaBox size={20} className="mr-2" />
           <span>Products</span>
           <span className="ml-auto text-gray-400">300</span>
+        </button>
+        <button onClick={()=> router.push(`${pathName}/add-products`)} className="mb-4 flex items-center text-gray-600">
+          <MdOutlineFileUpload size={20} className="mr-2" />
+          <span>Add New Products</span>
         </button>
         <button className="mb-4 flex items-center text-gray-600">
           <FaShoppingCart size={20} className="mr-2" />
--- app/vendor-account/page.tsx
+++ app/vendor-account/page.tsx
@@ -1,27 +1,20 @@
-import AppWrapper from "../AppWrapper";
+import VendorWrapper from "./components/VendorWrapper";
 import DashboardCards from "./components/dashboardCards";
 import SalesChart from "./components/salseChart";
-import SidebarMenu from "./components/sidebarMenu";
 import TopCountries from "./components/topCountries";
 
-const VendorAccount = () => {
+const page = () => {
   return (
-    <AppWrapper>
-      <div className="flex gap-4 container p-6">
-        <div className="w-1/4">
-          <SidebarMenu />
-        </div>
-        <div className="w-3/4">
-          <h1 className="mb-6 text-2xl font-bold">Dashboard</h1>
-          <DashboardCards />
-          <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-            <SalesChart /> 
-            <TopCountries />
-          </div>
-        </div>
+    <VendorWrapper>
+      <h1 className="mb-6 text-2xl font-bold">Dashboard</h1>
+      <DashboardCards />
+
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+        <SalesChart />
+        <TopCountries />
       </div>
-    </AppWrapper>
+    </VendorWrapper>
   );
 };
 
-export default VendorAccount;
+export default page;
